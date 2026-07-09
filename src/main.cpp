144316739126d818dbbe4b885173bd11e9a590c0
Mizux Seiha <mizux.dev@gmail.com>
2026-07-09 19:13:34 +0200

    sdl main use callback

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,17 @@
 #include <iostream>
 #include <numbers>
 
+#define SDL_MAIN_USE_CALLBACKS 1  // Tell SDL to use the callback architecture
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #include <GLES3/gl3.h> // Emscripten provides WebGL2 symbols natively here
-#include <functional>
-static std::function<void()> MainLoopForEmscriptenP;
-static void MainLoopForEmscripten() { MainLoopForEmscriptenP(); }
-#define EMSCRIPTEN_MAINLOOP_BEGIN MainLoopForEmscriptenP = [&]() { do
-#define EMSCRIPTEN_MAINLOOP_END                                                \
-  while (0)                                                                    \
-    ;                                                                          \
-  }                                                                            \
-  ;                                                                            \
-  emscripten_set_main_loop(MainLoopForEmscripten, 0, true);
 #else
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL3/SDL_opengl.h>
 #include <SDL3/SDL_opengl_glext.h>
-#define EMSCRIPTEN_MAINLOOP_BEGIN while (running)
-#define EMSCRIPTEN_MAINLOOP_END
 #endif
 
 // Simple matrix math helpers (to avoid external dependencies like GLM for this example)
@@ -125,163 +115,167 @@ GLuint compileShader(GLenum type, const char *source) {
 }
 
 // Main code
-int main(int, char **) {
-  // Setup SDL
-  // [If using SDL_MAIN_USE_CALLBACKS: all code below until the main loop starts
-  // would likely be your SDL_AppInit() function]
-  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
-    std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-    return 1;
-  }
+// A struct to hold your application state (replaces global variables)
+struct AppState {
+    SDL_Window* window = nullptr;
+    SDL_GLContext gl_context = nullptr;
+
+    GLuint shaderProgram;
+    GLuint VAO, VBO, EBO;
+    GLint mvpLoc;
+};
+
+// 1. Called once when the app starts. Initialize everything here.
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+    // Allocate our custom state structure
+    AppState* state = new AppState();
+    *appstate = state;
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
+        SDL_Log("SDL Initialization failed: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
 
 #ifdef __EMSCRIPTEN__
-  // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
-  const char* glsl_version = "#version 300 es";
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #else
-  // GL 3.0 + GLSL 130
-  const char* glsl_version = "#version 130";
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    // GL 3.0 + GLSL 130
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-  // Create window with graphics context
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-  float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
-  SDL_WindowFlags window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE |
-                                 SDL_WINDOW_HIDDEN |
-                                 SDL_WINDOW_HIGH_PIXEL_DENSITY;
-  SDL_Window *window =
-      SDL_CreateWindow("SDL3+OpenGL3 example", (int)(1280 * main_scale),
-                       (int)(800 * main_scale), window_flags);
-  if (window == nullptr) {
-    std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
-    SDL_Quit();
-    return 1;
-  }
-  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-  if (gl_context == nullptr) {
-    std::cerr << "OpenGL Context creation failed: " << SDL_GetError()
-              << std::endl;
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 1;
-  }
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    SDL_WindowFlags window_flags =
+      SDL_WINDOW_OPENGL |
+      SDL_WINDOW_RESIZABLE |
+      SDL_WINDOW_HIDDEN |
+      SDL_WINDOW_HIGH_PIXEL_DENSITY;
 
-  // Enable Depth Testing for 3D
-  glEnable(GL_DEPTH_TEST);
-
-  // SHADER SETUP START
-  // Compile and Link Shaders
-  GLuint vs = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
-  GLuint fs = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
-  GLuint shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vs);
-  glAttachShader(shaderProgram, fs);
-
-  glLinkProgram(shaderProgram);
-  {
-    GLint success;
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-      char infoLog[512];
-      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-      std::cerr << "SHADER PROGRAM LINKING ERROR:\n" << infoLog << std::endl;
+    state->window = SDL_CreateWindow("SDL3 Cross-Platform App", (int)(1280 * main_scale), (int)(800 * main_scale), window_flags);
+    if (state->window == nullptr) {
+      SDL_Log("Window creation failed: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
-  }
 
-  glDeleteShader(vs);
-  glDeleteShader(fs);
+    state->gl_context = SDL_GL_CreateContext(state->window);
+    if (state->gl_context == nullptr) {
+      SDL_Log("OpenGL Context creation failed: %s", SDL_GetError());
+      SDL_DestroyWindow(state->window);
+      return SDL_APP_FAILURE;
+    }
 
-  // Cube Vertices: Position (X,Y,Z) and Color (R,G,B)
-  float vertices[] = {
+    // Enable Depth Testing for 3D
+    glEnable(GL_DEPTH_TEST);
+
+    // SHADER SETUP START
+    // Compile and Link Shaders
+    GLuint vs = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fs = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+    state->shaderProgram = glCreateProgram();
+    glAttachShader(state->shaderProgram, vs);
+    glAttachShader(state->shaderProgram, fs);
+    glLinkProgram(state->shaderProgram);
+    {
+      GLint success;
+      glGetProgramiv(state->shaderProgram, GL_LINK_STATUS, &success);
+      if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(state->shaderProgram, 512, NULL, infoLog);
+        std::cerr << "SHADER PROGRAM LINKING ERROR:\n" << infoLog << std::endl;
+      }
+    }
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    // Cube Vertices: Position (X,Y,Z) and Color (R,G,B)
+    float vertices[] = {
       // Front face          // Colors
       -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, // BL
       0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,  // BR
       0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,   // TR
       -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,  // TL
-      // Back face
+                                            // Back face
       -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 1.0f, // BL
       0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f,  // BR
       0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f,   // TR
       -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, 0.0f   // TL
-  };
+    };
 
-  unsigned int indices[] = {
+    unsigned int indices[] = {
       0, 1, 2, 2, 3, 0, // Front
       1, 5, 6, 6, 2, 1, // Right
       7, 6, 5, 5, 4, 7, // Back
       4, 0, 3, 3, 7, 4, // Left
       4, 5, 1, 1, 0, 4, // Bottom
       3, 2, 6, 6, 7, 3  // Top
-  };
+    };
 
-  GLuint VAO, VBO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &state->VAO);
+    glGenBuffers(1, &state->VBO);
+    glGenBuffers(1, &state->EBO);
 
-  glBindVertexArray(VAO);
+    glBindVertexArray(state->VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, state->VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, state->EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Position Attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    // Color Attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_STATIC_DRAW);
+    state->mvpLoc = glGetUniformLocation(state->shaderProgram, "uMVP");
+    // SHADER SETUP END
 
-  // Position Attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-  // Color Attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
+    SDL_GL_MakeCurrent(state->window, state->gl_context);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+    SDL_SetWindowPosition(state->window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(state->window);
 
-  GLint mvpLoc = glGetUniformLocation(shaderProgram, "uMVP");
-  // SHADER SETUP END
+    return SDL_APP_CONTINUE; // Tells SDL to keep running
+}
 
-  SDL_GL_MakeCurrent(window, gl_context);
-  SDL_GL_SetSwapInterval(1); // Enable vsync
-  SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-  SDL_ShowWindow(window);
+// 2. Called whenever the system fires an event (input, window resize, etc.)
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+    AppState* state = static_cast<AppState*>(appstate);
 
-  // Main loop
-  bool running = true;
-  EMSCRIPTEN_MAINLOOP_BEGIN {
-    // Poll and handle events (inputs, window resize, etc.)
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_EVENT_QUIT)
-        running = false;
-      if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
-          event.window.windowID == SDL_GetWindowID(window))
-        running = false;
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS; // Safely exits the application
     }
-
-    // [If using SDL_MAIN_USE_CALLBACKS: all code below would likely be your
-    // SDL_AppIterate() function]
-    if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) {
-      SDL_Delay(10);
-      continue;
+    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
+        event->window.windowID == SDL_GetWindowID(state->window)) {
+        return SDL_APP_SUCCESS; // Safely exits the application
     }
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        if (event->key.scancode == SDL_SCANCODE_ESCAPE) {
+            return SDL_APP_SUCCESS;
+        }
+    }
+    return SDL_APP_CONTINUE;
+}
 
-    // Rendering
-    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+// 3. Called every single frame. This is your update and render loop.
+SDL_AppResult SDL_AppIterate(void* appstate) {
+    AppState* state = static_cast<AppState*>(appstate);
 
-    glUseProgram(shaderProgram);
-
-    // Build MVP Matrix
+    // --- 1. Update Game/App Logic Here ---
     float time = SDL_GetTicks() / 1000.0f;
 
+    // --- 2. Render Graphics Here ---
+    // Build MVP Matrix
     float model[16];
     get_rotation_y(time, model); // Rotate around Y axis over time
 
@@ -293,7 +287,7 @@ int main(int, char **) {
     };
 
     float projection[16];
-    get_projection(45.0f * (std::numbers::pi / 180.0f), 800.0f / 600.0f, 0.1f,
+    get_projection(45.0f * (std::numbers::pi / 180.0f), 1280.0f / 800.0f, 0.1f,
                    100.0f, projection);
 
     float viewProj[16];
@@ -302,27 +296,35 @@ int main(int, char **) {
     multiply_matrix(viewProj, model, mvp);
 
     // Pass matrix to shader
-    glUniformMatrix4fv(mvpLoc, 1, GL_TRUE, mvp);
+    glUseProgram(state->shaderProgram);
+    glUniformMatrix4fv(state->mvpLoc, 1, GL_TRUE, mvp);
 
     // Draw Cube
-    glBindVertexArray(VAO);
+    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBindVertexArray(state->VAO);
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
-    SDL_GL_SwapWindow(window);
-  }
-  EMSCRIPTEN_MAINLOOP_END
+    SDL_GL_SwapWindow(state->window);
 
-  // Cleanup
-  // [If using SDL_MAIN_USE_CALLBACKS: all code below would likely be your
-  // SDL_AppQuit() function]
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
-  glDeleteProgram(shaderProgram);
+    return SDL_APP_CONTINUE; // Keep looping
+}
 
-  SDL_GL_DestroyContext(gl_context);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+// 4. Called once automatically right before the program terminates
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+    if (appstate) {
+        AppState* state = static_cast<AppState*>(appstate);
 
-  return 0;
+        glDeleteVertexArrays(1, &state->VAO);
+        glDeleteBuffers(1, &state->VBO);
+        glDeleteBuffers(1, &state->EBO);
+        glDeleteProgram(state->shaderProgram);
+
+        SDL_GL_DestroyContext(state->gl_context);
+        SDL_DestroyWindow(state->window);
+        delete state; // Clean up our heap memory
+    }
+    SDL_Quit();
+    SDL_Log("Application shut down clean with code: %d", result);
 }
